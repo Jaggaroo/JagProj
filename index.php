a7651d00f278c602b4d966efaad0875f11485339
jagg1 <jagg00001@yahoo.ca>
2018-06-18 21:09:10 -0300

    New greeting to James

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+echo "Hi James"
+?>
+Something is working with the XAMPP installation :-)
