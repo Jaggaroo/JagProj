db635e303e7d1182f8453e201b238aa551b34dd9
Jagg2 <jamesagcool@hotmail.com>
2018-06-20 15:48:32 -0400

    Created basic registration page

--- Registration.php
+++ Registration.php
@@ -1,3 +1,12 @@
 <?php
-    echo "Registration placeholder page"
+    
 ?>
+<form method = "POST">
+    Last Name:<input type = "text" name = "User_LastName"><br>
+    First Name:<input type = "text" name = "User_FirstName"><br>
+    Rank:<input type = "text" name = "User_Rank"><br>
+    Unit:<input type = "text" name = "user_Unit"><br>
+    Password:<input type = "password" name = "user_password"><br>
+    Re-Enter Password:<input type = "password" name = "user_password2"><br>
+    <input type = "submit" name = "submit" value = "submit">
+</form>
