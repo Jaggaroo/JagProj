6546e130f7a7b21b3eac04468846669799c67d69
jagg1 <jagg00001@yahoo.ca>
2018-06-20 16:47:56 -0300

    css directory display and first css

--- display.php
+++ display.php
@@ -3,6 +3,28 @@
 
 ?>
 
-<p>This is where the actual action happens</p>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+<meta name="description" content="Sea cadet quiz program">
+<meta name="author" content="John Green PO1 ret'd">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Main Display</title>
+<link rel="stylesheet" href="css/cadetQuiz.css">
+
+</head>
+<body>
+
+<h1>This is where the actual action happens</h1>
 <p>But it needs work</p>
+
+
+
+</body>
+</html> 
+
+
 
