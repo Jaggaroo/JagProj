33856bedc7255ec69d27db9d19a9c888b2b6bc44
Jagg2 <jamesagcool@hotmail.com>
2018-06-20 15:44:13 -0400

    Added simple user and password fields mothod is set to post and has a hubmit button

--- Login.php
+++ Login.php
@@ -1,3 +1,9 @@
 <?php
     echo "Login placeholder page"
 ?>
+
+<form method = "POST">
+    Username:<input type = "text" name = "user_name"><br>
+    Password:<input type = "password" name = "user_password"><br>
+    <input type = "submit" name = "submit" value = "submit">
+</form>
